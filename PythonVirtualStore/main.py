a2f5a3094e3102a88c9bcb9eb71f843e54613811
Azam Ahmed <aziskool20@gmail.com>
2021-10-05 12:25:26 -0400

    python: Added buy item functionality and other minor improvements

--- PythonVirtualStore/main.py
+++ PythonVirtualStore/main.py
@@ -3,6 +3,7 @@ from item.item import Item
 import json
 
 items = []
+items_bought = []
 
 # Set the default items
 def set_default_items():
@@ -13,16 +14,20 @@ def set_default_items():
   items.append(Item("Chair", Category.HOME_AND_FURNITURE, 40284, 105.99))
 
 # Convert the items to json format
-def get_all_items():
+def get_all_items(list_name, inJson):
   json_items = []
-  for item in items:
+  for item in list_name:
     json_items.append(json.dumps(item.__dict__))
-  return json_items
+  if (inJson):
+    return json_items
+  return items
 
-def get_item_by_id(id):
-  for item in items:
+def get_item_by_id(list_name, id, inJson):
+  for item in list_name:
     if item.id == id:
-      return json.dumps(item.__dict__)
+      if (inJson):
+        return json.dumps(item.__dict__)
+      return item
 
 def create_item(name, category, quantity, price):
   items.append(Item(name, category, quantity, price))
@@ -32,7 +37,20 @@ def delete_item(id):
     if item.id == id:
       items.remove(item)
 
+def buy_item(id):
+  for item in items:
+    if item.id == id:
+      item.quantity -= 1
+  items_bought.append(get_item_by_id(items, id, False))
+
+
+# Buy item
 set_default_items()
+print(f"All items: {get_all_items(items, True)}")
+item_to_buy_id = input("Item id: ")
+buy_item(item_to_buy_id)
+print(f"Items bought: {get_all_items(items_bought, True)}")
+print(f"All items: {get_all_items(items, True)}")
 # create_item("Shoe Dog", Category.BOOKS, 432234, 14.99)
 # print(get_all_items())
 
